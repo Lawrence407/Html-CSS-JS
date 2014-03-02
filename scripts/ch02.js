49d331f5cf929d9834edc0663b530779e8412a93
PinkingS <405768386@qq.com>
2014-03-02 10:33:51 +0800

    update

---
+++ scripts/ch02.js
@@ -0,0 +1,11 @@
+
+			$(document).ready(function() {
+				$(".guess_box").click( function() {
+					var discount = Math.floor((Math.random()*5) + 5);
+					var discount_msg = "<p>Your Discount is "+discount+"%</p>";
+					//$(".guess_box p").remove();
+					$(this).append(discount_msg);
+					$(".guess_box").unbind("click");
+				});//yes this is a comment
+				
+			});
